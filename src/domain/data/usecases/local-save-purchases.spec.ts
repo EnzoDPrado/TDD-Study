71959beaf3b47f276bacc2d46c8ef96e8b338119
EnzoDPrado <enzo.prado@qodeless.io>
2024-06-04 23:01:06 -0300

    just delete if the key is purchases

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -2,19 +2,20 @@ class LocalSavePurchases{
     constructor (private readonly cacheStore: CacheStore){}
 
     async save(): Promise<void>{
-        this.cacheStore.delete()
+        this.cacheStore.delete('purchases')
     }
 }
 
 interface CacheStore{
-    delete: () => void
+    delete: (key: string) => void
 }
 
 class CacheStoreSpy implements CacheStore{
     deleteCallsCount = 0
-
-    delete (): void{
+    key: string
+    delete (key: string): void{
         this.deleteCallsCount++
+        this.key = key
     }
 }
 
@@ -42,11 +43,6 @@ describe ('LocalSavePurchases', () => {
         const {cacheStore, sut} = makeSut()
         await sut.save()
         expect(cacheStore.deleteCallsCount).toBe(1)
-    })
-
-    test('Should delete old cache on sut.save', async () => {
-        const {cacheStore, sut} = makeSut()
-        await sut.save()
-        expect(cacheStore.deleteCallsCount).toBe(1)
+        expect(cacheStore.key).toBe('purchases')
     })
 })
