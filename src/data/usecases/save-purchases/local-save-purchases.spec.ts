7f8ff67a4d59306490b97e8a3b4a7936563293cd
EnzoDPrado <enzo.prado@qodeless.io>
2024-06-05 08:51:03 -0300

    ensure local save purchases insert cache with the correct key

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -4,11 +4,16 @@ import {LocalSavePurchases} from '@/data/usecases'
 class CacheStoreSpy implements CacheStore{
     deleteCallsCount = 0
     insertCallsCount = 0
-    key: string
+    deleteKey: string
+    insertKey: string
+    insert (key: string): void {
+        this.insertCallsCount++
+        this.insertKey = key
+    }
 
     delete (key: string): void{
         this.deleteCallsCount++
-        this.key = key
+        this.deleteKey = key
     }
 }
 
@@ -36,14 +41,24 @@ describe ('LocalSavePurchases', () => {
         const {cacheStore, sut} = makeSut()
         await sut.save()
         expect(cacheStore.deleteCallsCount).toBe(1)
-        expect(cacheStore.key).toBe('purchases')
+        expect(cacheStore.deleteKey).toBe('purchases')
     })
 
-    test('Should not insert new Cache if delete fails', async () => {
+    test('Should not insert new Cache if delete fails', () => {
         const {cacheStore, sut} = makeSut()
         jest.spyOn(cacheStore, 'delete').mockImplementationOnce(() => { throw new Error()})
         const promisse = sut.save()
         expect(cacheStore.insertCallsCount).toBe(0)
         expect(promisse).rejects.toThrow()
     })
+
+    test('Should insert new cache if delete succeeds', async () => {
+        const {cacheStore, sut} = makeSut()
+        await sut.save()
+        expect(cacheStore.deleteCallsCount).toBe(1)
+        expect(cacheStore.insertCallsCount).toBe(1)
+        expect(cacheStore.insertKey).toBe('purchases')
+    })
+
+
 })
